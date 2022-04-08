bbb2e14d054eafcb9d33c335150588be1c105459
gmmour1 <gmmour@gmail.com>
2022-04-08 20:03:55 +0400

    expect to be rejected for yi token invalid price type test

--- tests/utils.ts
+++ tests/utils.ts
@@ -1,6 +1,5 @@
 import { Program } from '@project-serum/anchor';
 import * as pythUtils from './mock_account_utils';
-import * as global from './global';
 
 export enum Tokens {
   SOL = 0,
@@ -52,42 +51,4 @@ export async function createFakeAccounts(fakePythProgram: Program<any>, initialT
       }
     })
   );
-}
-
-/**
- * Get the custom program error code if there's any in the error message and return parsed error code hex to number string
- * @param errMessage string - error message that would contain the word "custom program error:" if it's a customer program error
- * @returns [boolean, string] - probably not a custom program error if false otherwise the second element will be the code number in string
- */
-export const getCustomProgramErrorCode = (errMessage: string): [boolean, string] => {
-  const index = errMessage.indexOf('Custom program error:');
-  if (index == -1) {
-    return [false, 'May not be a custom program error'];
-  } else {
-    return [true, `${parseInt(errMessage.substring(index + 22, index + 28).replace(' ', ''), 16)}`];
-  }
-};
-
-/**
- *
- * Maps the private Anchor type ProgramError to a normal Error.
- * Pass ProgramErr.msg as the Error message so that it can be used with chai matchers
- *
- * @param fn - function which may throw an anchor ProgramError
- */
-export async function mapAnchorError<T>(fn: Promise<T>): Promise<T> {
-  try {
-    return await fn;
-  } catch (e: any) {
-    let [isCustomProgramError, errorCode] = getCustomProgramErrorCode(JSON.stringify(e));
-    if (isCustomProgramError) {
-      let error: any;
-      if (Number(errorCode) >= 6000 && Number(errorCode) <= 7000) {
-        errorCode[errorCode.length - 2] === '0' ? (errorCode = errorCode.slice(-1)) : (errorCode = errorCode.slice(-2));
-        error = global.ScopeIdl.errors[errorCode].msg;
-        throw new Error(error);
-      }
-    }
-    throw e;
-  }
 }
