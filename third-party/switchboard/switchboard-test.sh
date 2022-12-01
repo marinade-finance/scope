269d4c52cd335ae2b7ea8573e59f37f39d311c68
Elliot Kennedy <me@elken.io>
2022-12-01 12:47:28 +0000

    Add mSOL and stSOL swtichboard v2 oracles (#91)

--- third-party/switchboard/switchboard-test.sh
+++ third-party/switchboard/switchboard-test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl https://api.switchboard.xyz/api/test -X POST -H "Content-Type: application/json" -d "@$1"
+curl https://api.switchboard.xyz/api/test -X POST -H "Content-Type: application/json" -d "@$1" | python -m json.tool
